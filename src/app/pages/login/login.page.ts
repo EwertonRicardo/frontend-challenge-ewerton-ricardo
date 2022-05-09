a722888abff58de7cede0672e8e06894141827d4
ewerton <ewerton.ricardo98@gmail.com>
2022-05-09 20:31:22 -0300

    feat: created the loginForm

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this._createForm()
+  }
+
+  private _createForm(): void {
+    this.loginForm = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required, Validators.min(6)]
+    })
   }
 
 }
