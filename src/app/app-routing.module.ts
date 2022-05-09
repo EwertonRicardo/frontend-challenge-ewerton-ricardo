ddaa095e0af9bbbcf987f17c9ee327644ad8d8b9
ewerton <ewerton.ricardo98@gmail.com>
2022-05-09 20:18:50 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./pages/countries/countries.module').then( m => m.CountriesPageModule)
+  },
+  {
+    path: 'country-detail',
+    loadChildren: () => import('./pages/country-detail/country-detail.module').then( m => m.CountryDetailPageModule)
+  },
 ];
 
 @NgModule({
